116fa4e45b83cfa196750136aa69f1dadd18fdaa
Jessica Dussault <jdussault4@gmail.com>
2017-08-08 15:31:18 -0500

    cleaning up UX just a little bit

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,32 @@
 current_office = "All";
+// sets up d3 data
+var initial = contracts[0];
+var data = JSON.parse(JSON.stringify(initial));
+update("All", data);
+
 
 function changeOffice(office) {
   document.getElementById("officeTitle").innerHTML = office;
   var hiring_office_contracts = contracts.find(function(table) { return table.office == office });
   update(office, hiring_office_contracts);
-
 }
 
-// d3 stuff here
-var initial = contracts[0];
-var data = JSON.parse(JSON.stringify(initial));
-update("All", data);
+function select_office_button(office) {
+  buttons = document.getElementsByClassName("btn-office");
+  buttonLength = buttons.length;
+  for (var i = 0; i < buttonLength; i++) {
+    var btn = buttons[i];
+    if (btn.innerHTML == office) {
+      btn.className += " active";
+    } else {
+      btn.className = btn.className.replace(" active", "");
+    }
+  }
+}
 
 function update(office, data) {
+  select_office_button(office);
   updateMap(current_office, office);
   updateTable([data]);
   current_office = office;
-};
+}
