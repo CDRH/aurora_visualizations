086b0f824f9663ef3073cb23acfa027648b74098
Karin Dalziel <kdalziel2@unl.edu>
2017-08-22 09:24:06 -0500

    map samples for will

--- assets/map_alt2.js
+++ assets/map_alt2.js
@@ -68,7 +68,7 @@ function updateMap(current, requested) {
 // 'http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png'
 
 
-var osmLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}', {
+var osmLayer = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}.{ext}', {
   attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: 'abcd',
   minZoom: 0,
